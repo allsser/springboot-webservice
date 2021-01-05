8b08669b08484187412085771980b253d339e43c
allsser <allsser@naver.com>
2021-01-05 16:20:12 +0900

    버전 문제 였다. Spring Boot 2.4.1 Gradle 6.7.1 Intellij IDEA 2020.3 Junit5

--- src/test/java/com/allsser/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/allsser/book/springboot/web/HelloControllerTest.java
@@ -1,18 +1,18 @@
 package com.allsser.book.springboot.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class, secure = false)  //401오류가 계속 뜸, secure = false 추가해서 시큐리티 위존성 해제
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)  //401오류가 계속 뜸, secure = false 추가해서 시큐리티 위존성 해제
 public class HelloControllerTest {
 
     @Autowired
